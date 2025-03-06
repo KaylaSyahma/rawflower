5e3f710931a8fd844b18f1e237e7a412c28b94a7
KaylaSyahma <ksyahma22@gmail.com>
2025-03-06 07:48:18 +0700

    first commit

---
+++ app/Models/User.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasName;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable implements FilamentUser, HasName
+{
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role_as', // role user (0: user biasa, 1: admin)
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+            'role_as' => 'integer', // biar role langsung dikonversi jadi integer
+        ];
+    }
+
+    /**
+     * Cek apakah user adalah admin
+     * @return bool
+     */
+    public function isAdmin()
+    {
+        return $this->role_as == 1; // kalo role_as 1, berarti admin
+    }
+
+    /**
+     * Untuk akses admin di Filament
+     * @return bool
+     */
+    public function canAccessPanel($panel): bool
+    {
+        return $this->isAdmin(); // cuma admin yang bisa akses Filament
+    }
+
+    /**
+     * Get name buat di Filament
+     * @return string
+     */
+    public function getFilamentName(): string
+    {
+        return $this->name;
+    }
+}
