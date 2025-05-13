2c49dd3ffebed830551432c88b96e44be6215913
ShikaaDhia <shkaadhier@gmail.com>
2025-05-13 20:16:08 +0700

    ini signin/signup

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Api\ProductController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use Illuminate\Http\Request;
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,7 +16,7 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', [RegisteredUserController::class, 'register']);
 Route::post('/login', [AuthenticatedSessionController::class, 'login']);
 
-// Route untuk mendapatkan data user yang sedang login (Authenticated User)
+// route untuk mendapatkan data user yang sedang login (authenticated User)
 Route::middleware('auth:sanctum')->get('user', function (Request $request) {
     return response()->json($request->user());
 });
@@ -30,7 +31,7 @@ Route::apiResource('/categories', CategoryController::class);
 Route::get('/products/popular', [ProductController::class, 'popular']);
 //buat detail
 Route::get('/products/{id}/detail', [ProductController::class, 'showDetail'])->name('detail');
-// api resource harus ditaro di akhir!!!!!!
+// api resource harus ditaro di akhir!
 Route::apiResource('/products', ProductController::class);
 
 Route::apiResource('/blogs', BlogController::class);
