2a21884fb667fb04246d11de08e1027917578ea9
KaylaSyahma <ksyahma22@gmail.com>
2025-05-25 09:30:06 +0700

    fixing cors

--- config/cors.php
+++ config/cors.php
@@ -1,1 +1,21 @@
-git add 
+<?php
+
+return [
+
+    'paths' => ['api/*', 'login', 'logout', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
